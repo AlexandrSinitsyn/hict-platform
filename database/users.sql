a7f4214c2b4696cc68e7ee6d85d51df3cbad1477
Sasha Sinitsyn <Sannitsa@yahoo.com>
2024-01-14 02:49:52 +0300

    new(db): viewer user

---
+++ database/users.sql
@@ -0,0 +1,11 @@
+create user viewer with
+    login
+    nosuperuser
+    nocreatedb
+    nocreaterole
+    noinherit
+    noreplication
+    password '${POSTGRES_VIEWER_PASSWORD}';
+grant usage on schema public to viewer;
+grant select on all tables in schema public to viewer;
+alter default privileges in schema public grant select on tables to viewer;
