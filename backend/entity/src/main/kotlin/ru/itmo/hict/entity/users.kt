000170225369c2ca8639ce2ebe9d3e299276ffb6
Sasha Sinitsyn <Sannitsa@yahoo.com>
2024-01-14 02:47:47 +0300

    upd(db): init sql

--- backend/entity/src/main/kotlin/ru/itmo/hict/entity/users.kt
+++ backend/entity/src/main/kotlin/ru/itmo/hict/entity/users.kt
@@ -3,6 +3,7 @@ package ru.itmo.hict.entity
 import jakarta.persistence.*
 import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 import org.hibernate.annotations.CreationTimestamp
 import java.sql.Timestamp
 
@@ -14,14 +15,55 @@ enum class Role {
 }
 
 @Entity
-@Table(name = "users")
-class User(
-    @NotNull @NotBlank                                                  val username: String,
-    @NotNull @NotBlank                                                  val login: String,
-    @NotNull @NotBlank                                                  val email: String,
-    @NotNull @NotBlank                                                  val passwordSha: String,
-    @NotNull                                                            val role: Role,
-    @NotNull @GeneratedValue(strategy = GenerationType.IDENTITY)    @Id val id: Long? = null,
-    @NotNull                                         @CreationTimestamp val creationTime: Timestamp? = null,
+@Table(
+    name = "users",
+    uniqueConstraints = [
+        UniqueConstraint(columnNames = ["id"]),
+        UniqueConstraint(columnNames = ["login"]),
+        UniqueConstraint(columnNames = ["email"]),
+    ],
+    indexes = [
+        Index(name = "users_by_id", columnList = "user_id", unique = true),
+        Index(name = "users_by_login", columnList = "login,user_id", unique = true),
+        Index(name = "users_by_email", columnList = "email,user_id", unique = true),
+    ],
 )
+class User(
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @Column(name = "username", nullable = false)
+    val username: String,
 
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 64)
+    @Column(name = "login", unique = true, nullable = false)
+    val login: String,
+
+    @NotNull
+    @NotBlank
+    @Size(min = 3, max = 100)
+    @Column(name = "email", unique = true, nullable = false)
+    val email: String,
+
+    @NotNull
+    @NotBlank
+    @Column(name = "password_sha", nullable = false)
+    val passwordSha: String,
+
+    @NotNull
+    @Column(name = "role", columnDefinition = "TINYINT", nullable = false)
+    val role: Role,
+
+    @NotNull
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @Column(name = "user_id", nullable = false)
+    val id: Long? = null,
+
+    @NotNull
+    @CreationTimestamp
+    @Column(name = "creation_time", nullable = false)
+    val creationTime: Timestamp? = null,
+)
---
+++ database/init.sql
@@ -0,0 +1,38 @@
+create extension if not exists pgcrypto;
+
+create table users
+(
+    user_id       bigint                   not null generated always as identity,
+    username      varchar(100)             not null,
+    login         varchar(64)              not null,
+    email         varchar(100)             not null,
+    role          smallint                 not null check ( 0 <= role and role <= 3 ),
+    salt          varchar(30)              not null,
+    password_sha  varchar(60)              not null,
+    creation_time timestamp with time zone not null default now(),
+    primary key (user_id),
+    unique (user_id),
+    unique (login),
+    unique (email)
+);
+
+create index users_by_id on users using hash (user_id) ;
+create unique index users_by_login on users using btree (login, user_id) ;
+create unique index users_by_email on users using btree (email, user_id) ;
+
+create table hi_c_maps
+(
+    hi_c_map_id   bigint                   not null,
+    user_id       bigint                   not null,
+    name          varchar(100)             not null,
+    description   text                     not null,
+    creation_time timestamp with time zone not null default now(),
+    primary key (hi_c_map_id),
+    unique (hi_c_map_id),
+    unique (name),
+    constraint fk_author_id foreign key (user_id) references users (user_id)
+);
+
+create index hi_c_map_by_id on hi_c_maps using hash (hi_c_map_id) ;
+create index hi_c_map_by_name on hi_c_maps using hash (name) ;
+create unique index hi_c_map_by_author on hi_c_maps using btree (user_id, hi_c_map_id) ;
