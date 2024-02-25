41be0706ca17f7ffc4253612107ec516adf2fac0
Sasha Sinitsyn <Sannitsa@yahoo.com>
2024-02-26 01:38:20 +0300

    upd: project cleanup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,12 @@
+import '/public/css/normalize.css';
 import '/public/css/main.css';
+import 'jquery/dist/jquery.slim';
+import 'vue3-toastify/dist/index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { createApp } from 'vue';
 import App from './App.vue';
 
 createApp(App).mount('#app');
+
+import 'bootstrap-icons/font/bootstrap-icons.css';
