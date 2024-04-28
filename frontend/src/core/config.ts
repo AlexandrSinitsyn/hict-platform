c487b590b6407be80f588b7fb101a7624288b8b1
Sasha Sinitsyn <Sannitsa@yahoo.com>
2024-04-28 14:38:34 +0300

    new!: HiCT server host storage

--- frontend/src/core/config.ts
+++ frontend/src/core/config.ts
@@ -6,9 +6,11 @@ export const __VERSION__: string = process.env?.VITE_PROJECT_VERSION ?? '0.1';
 
 export const __AUTH_HOST__: string = process.env?.VITE_AUTH_HOST ?? 'unknown';
 export const __SERVER_HOST__: string = process.env?.VITE_SERVER_HOST ?? 'unknown';
+export const __HiCT_CLUSTER__: string = process.env?.VITE_HICT_CLUSTER_HOST ?? 'unknown';
 
 export const AUTH = `http://${__AUTH_HOST__}/api/v1`;
 export const SERVER = `http://${__SERVER_HOST__}/api/v1`;
+export const HiCT = `http://${__HiCT_CLUSTER__}`;
 
 export function notify(type: 'info' | 'success' | 'warning' | 'error', message: string) {
     toast(message, {
--- frontend/src/view/HiCView.vue
+++ frontend/src/view/HiCView.vue
@@ -4,14 +4,11 @@
 
         <div v-if="selected">
             <p>Name: {{ selected.meta.name }}</p>
-            <p>Description: {{ selected.meta.description }}</p>
-            <p>Created at: {{ selected.meta.creationTime }}</p>
-            <p>Views: {{ selected.views }}</p>
+<!--            <p>Description: {{ selected.meta.description }}</p>-->
+<!--            <p>Created at: {{ selected.meta.creationTime }}</p>-->
+<!--            <p>Views: {{ selected.views }}</p>-->
 
-            <div class="btn btn-primary" @click="gen">
-                <span>Generate random number</span>
-            </div>
-            <p>Result: {{ randomNumber }}</p>
+            <App />
         </div>
         <div v-else>You have not selected any map yet!</div>
     </div>
@@ -19,33 +16,34 @@
 
 <script setup lang="ts">
 import { onMounted, type Ref, ref } from 'vue';
-import { acquireHiCMap, pingHiCMap, hictServerRequest } from '@/core/server-requests';
+import { acquireHiCMap, pingHiCMap } from '@/core/server-requests';
 import type { HiCMap } from '@types';
-import { notify } from "@/core/config";
-import { useAuthStore } from "@/stores/auth-store";
-import { storeToRefs } from "pinia";
+import { HiCT, notify } from '@/core/config';
+import { useAuthStore } from '@/stores/auth-store';
+import { storeToRefs } from 'pinia';
 import { useRoute } from 'vue-router';
+import { useRemoteHostStore } from '@hict/app/stores/remoteHost';
+
+import App from '@hict/App.vue';
 
 const { user } = storeToRefs(useAuthStore());
 const selected: Ref<HiCMap | undefined> = ref(undefined);
-const randomNumber = ref('?');
 
-function gen() {
+const doRequest = () => {
+    pingHiCMap(useRoute().params.hiCMapName as string);
+
     const uid = user.value?.id;
 
     if (!uid) {
         notify('error', 'You should be authorized');
-        return;
     }
 
-    pingHiCMap(useRoute().params.hiCMapName as string);
-
-    hictServerRequest(uid, (v) => {
-        randomNumber.value = v;
-    });
-}
+    return `${HiCT}`;//${uid}`;
+};
 
 onMounted(() => {
+    useRemoteHostStore().setDynamicRemoteHost(doRequest);
+
     const router = useRoute();
     acquireHiCMap(router.params.hiCMapName as string, (map) => {
         selected.value = map;
