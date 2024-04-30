a19c6a4c97f990c0c7167c11fa8b84f7fc7453ad
Sasha Sinitsyn <Sannitsa@yahoo.com>
2024-05-01 00:36:00 +0300

    fix(gRPC): docker

---
+++ backend/scheduler/src/main/kotlin/ru/itmo/hict/scheduler/config/GrpcConfig.kt
@@ -0,0 +1,25 @@
+package ru.itmo.hict.scheduler.config
+
+import net.devh.boot.grpc.common.autoconfigure.GrpcCommonCodecAutoConfiguration
+import net.devh.boot.grpc.server.autoconfigure.*
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
+import org.springframework.context.annotation.Configuration
+
+// net.devh is not supported yet for spring-boot 3
+// https://github.com/yidongnan/grpc-spring-boot-starter/pull/775#issuecomment-1329023335
+@Configuration
+@ImportAutoConfiguration(
+    GrpcCommonCodecAutoConfiguration::class,
+    GrpcAdviceAutoConfiguration::class,
+    GrpcHealthServiceAutoConfiguration::class,
+    GrpcMetadataConsulConfiguration::class,
+    GrpcMetadataEurekaConfiguration::class,
+    GrpcMetadataNacosConfiguration::class,
+    GrpcMetadataZookeeperConfiguration::class,
+    GrpcReflectionServiceAutoConfiguration::class,
+    GrpcServerAutoConfiguration::class,
+    GrpcServerFactoryAutoConfiguration::class,
+    GrpcServerMetricAutoConfiguration::class,
+    GrpcServerSecurityAutoConfiguration::class,
+)
+class GrpcConfig
