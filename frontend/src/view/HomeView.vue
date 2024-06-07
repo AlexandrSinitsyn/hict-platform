1f8e90cfc76431066dd53c7b7691225be50e6f2e
Sasha Sinitsyn <Sannitsa@yahoo.com>
2024-06-07 12:01:47 +0300

    move: experiments -> home

--- frontend/src/view/HomeView.vue
+++ frontend/src/view/HomeView.vue
@@ -1,40 +1,107 @@
 <template>
-    <div>
-        <img alt="Logo" class="logo" src="/public/img/icon.ico" width="125" height="125" />
-
+    <div v-if="!selectedExperiment">
         <h1>{{ __NAME__ }}</h1>
+        <h3>Experiments</h3>
 
-        <div class="auth">
-            <LoginFormComponent @submit="loginSubmit" />
+        <div class="experiments-filter">
+            <label for="experiments-filter">Filter:</label>
+            <input id="experiments-filter" type="text" />
+            <div class="btn btn-outline-primary" @click="newExperiment">Create new</div>
+        </div>
 
-            <RegisterFormComponent @submit="registerSubmit" />
+        <div class="experiments">
+            <div v-for="e in experiments" :key="e.name">
+                <div @click="selectedExperiment = e" class="view-experiment">
+                    [Experiment] {{ e.name }}
+                </div>
+                <div
+                    v-for="cm in e.contactMaps"
+                    :key="cm.name"
+                    @click="selectMap(cm)"
+                    class="view-contact-map"
+                    style="margin-left: 2rem"
+                >
+                    [Contact map] {{ cm.name }}
+                </div>
+                <div v-for="a in e.assemblies" :key="a.name" class="view-assembly">
+                    [Assembly] {{ a.name }}
+                </div>
+            </div>
         </div>
     </div>
+    <NewExperimentView v-if="selectedExperiment" :selected="selectedExperiment" />
 </template>
 
 <script setup lang="ts">
-import LoginFormComponent from '@/components/forms/LoginFormComponent.vue';
-import RegisterFormComponent from '@/components/forms/RegisterFormComponent.vue';
+import { onMounted, ref, type Ref } from 'vue';
+import type { ContactMap, Experiment } from '@types';
+import { getAllExperiments, publishExperiment } from '@/core/experiment-requests';
+import NewExperimentView from '@/view/NewExperimentView.vue';
+import router from '@/router';
 import { __NAME__ } from '@/core/config';
-import { getAuthorizedUser, login, register } from '@/core/authentication';
-import { useAuthStore } from '@/stores/auth-store';
-import type { LoginForm, RegisterForm } from '@types';
 
-const authStore = useAuthStore();
+const experiments: Ref<Experiment[]> = ref([]);
+const selectedExperiment: Ref<Experiment | undefined> = ref(undefined);
 
-function loginSubmit(form: LoginForm) {
-    login(form, () => getAuthorizedUser(authStore.login));
+onMounted(() => {
+    getAllExperiments((all: Experiment[]) => {
+        experiments.value = all;
+    });
+});
+
+function newExperiment() {
+    publishExperiment((experiment: Experiment) => {
+        selectedExperiment.value = experiment;
+    });
 }
 
-function registerSubmit(form: RegisterForm) {
-    register(form, () => getAuthorizedUser(authStore.login));
+function selectMap(map: ContactMap): void {
+    router.push({
+        name: 'view',
+        params: {
+            contactMapName: map.name,
+        },
+    });
 }
 </script>
 
 <style scoped lang="scss">
-.auth {
-    width: 15vw;
+@import '/public/css/main';
+
+.experiments {
+    padding: 1rem;
+    border: 1px solid $border-color;
+    border-radius: $border-radius;
+
+    @each $type, $color in ('view-experiment': $danger, 'view-contact-map': $success, 'view-assembly': $info) {
+        .#{$type} {
+            padding: 0.5rem;
+            color: $color;
+            font-weight: bolder;
+
+            &:hover {
+                background-color: opacity($color, 0.2);
+            }
+        }
+    }
+}
+
+.experiments-filter {
+    margin: 3rem;
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
+    flex-direction: row;
+
+    & > label {
+        margin: auto 0;
+    }
+
+    & > input {
+        width: 70%;
+        padding: 0.5rem;
+        border: 1px solid $border-color;
+        border-radius: $border-radius;
+        resize: none;
+    }
 }
 </style>
