7e76934eefedf5c1f8946c5632750777991ec35b
Sasha Sinitsyn <Sannitsa@yahoo.com>
2024-06-09 03:04:08 +0300

    fix(frontend)!: deploy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,7 +8,7 @@ export default ({ mode }: { mode: string }) => {
     const env = loadEnv(mode, process.cwd(), '');
 
     return defineConfig({
-        base: '/',
+        base: 'https://genome.ifmo.ru/hict/',
         define: {
             'process.env': env,
         },
@@ -21,7 +21,7 @@ export default ({ mode }: { mode: string }) => {
             },
         },
         server: {
-            port: 82,
+            port: 80,
         },
         build: {
             sourcemap: false,
